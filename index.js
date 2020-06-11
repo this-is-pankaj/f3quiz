d1583e1fd7bc298e2a7b86da8e64935d4b7924d9
Gupta <pankgupta@deloitte.com>
2020-06-11 13:09:06 +0530

    - added mongoconf options

--- index.js
+++ index.js
@@ -11,7 +11,16 @@ const component = 'index';
 
 mongoose.Promise = global.Promise;
 console.log(config.connectionString);
-mongoose.connect(config.connectionString)
+const options = {
+	useMongoClient: true,
+	// autoIndex: false, // Don't build indexes
+	reconnectTries: 100, // Never stop trying to reconnect
+	reconnectInterval: 500, // Reconnect every 500ms
+	poolSize: 10, // Maintain up to 10 socket connections
+	// If not connected, return errors immediately rather than waiting for reconnect
+	bufferMaxEntries: 0
+};
+mongoose.connect(config.connectionString, options)
   .then(() => {
     LOG.info(`${component}.mongoose`, 'defaultId', 'Database is connected');
   })
